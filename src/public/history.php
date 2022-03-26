688f408b887e772c88109b8464556cfae30550d0
YuDemura <Yu.demura1214@gmail.com>
2022-03-27 05:23:56 +0900

    送信履歴ページを実装しました。

---
+++ src/public/history.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>送信履歴</h1>
+
+<?php
+$title = $_POST['title'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$dbUserName = "root";
+$dbPassword = "password";
+$pdo = new PDO("mysql:host=mysql; dbname=contactform; charset=utf8", $dbUserName, $dbPassword);
+
+$sql = "select * from contacts";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($results as $row) {
+    echo $row["title"] . "<br>";
+    echo $row["content"] . "<br>";
+}
+?>
+
+        <a href="index.php">戻る</a>
+</body>
+</html>
