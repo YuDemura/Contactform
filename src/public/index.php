39f5a4c0a0084248e78553046b73a6eb2920cfb2
YuDemura <Yu.demura1214@gmail.com>
2022-03-27 05:22:31 +0900

    トップページを実装しました。

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,26 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!-- <?php
+// session_start();
+//     $_SESSION['title'] = $_POST['title'];
+//     $_SESSION['email'] = $_POST['email'];
+//     $_SESSION['message'] = $_POST['message'];
+//     header('Location: complete.php');
+?> -->
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>お問い合わせフォーム</title>
+</head>
+<body>
+    <form action="./complete.php" method="post">
+        <h1>お問い合わせフォーム</h1>
+        <p>以下のフォームからお問い合わせ下さい。</p>
+        タイトル　（必須） <input type="text" name="title" placeholder="タイトル"><br>
+        Email　（必須）　<input type="email" name="email" placeholder="Email アドレス"><br>
+        お問い合わせ内容　（必須）<br>
+        <textarea cols="100" rows="10" maxlength="1000"  name="message" placeholder="お問い合わせ内容（1000文字まで）をお書き下さい。"></textarea><br>
+        <input type="submit" name="send" value="送信" />
+    </form>
+</body>
+</html>
